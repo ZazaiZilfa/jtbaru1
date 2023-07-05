0a06739dda54ce542b98bd0c8ff81c509433fb60
ZazaiZilfa <zazaizilfa9@gmail.com>
2023-07-05 11:54:28 +0700

    Update logout.php

--- models/logout.php
+++ models/logout.php
@@ -1,5 +1,8 @@
 <?php
+session_start();
+require '.././include/fungsi.php';
+
 $_SESSION = [];
 session_unset();
 session_destroy();
-header("location: ../index");
+header("location: ../index.php");
